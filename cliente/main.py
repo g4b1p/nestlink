71e4efc6ca16c5f92029a48fea2e93a2879a8c79
gabi <plataramirez17@gmail.com>
2025-09-23 15:31:10 -0300

    cambio de pyqt5 a customtkinter

--- cliente/main.py
+++ cliente/main.py
@@ -1,20 +1,24 @@
-from PyQt5.QtWidgets import *
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
+import customtkinter
+
+# Crea una clase para la ventana principal de la aplicación.
+class MainApp(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+
+        # Configura la ventana principal.
+        self.title("Nestlink App")
+        self.geometry("500x300")
+
+        # Crea y muestra un mensaje de bienvenida.
+        welcome_label = customtkinter.CTkLabel(
+            self, 
+            text="Bienvenido a Nestlink", 
+            font=customtkinter.CTkFont(size=20, weight="bold")
+        )
+        welcome_label.pack(pady=50)
 
 
-class MainApp(QMainWindow):
-    def __init__(self, parent=None, *args):
-        super(MainApp, self).__init__(parent=parent)
-
-        self.setMinimumSize(500, 300)
-        #self.setMaximumSize(700, 500)
-        #self.setFixedSize(500, 300)
-        self.setWindowTitle("Nestlink App")
-
-        label = QLabel("Bienvenido a Nestlink", self)
-
-app = QApplication([])
-window = MainApp()
-window.show()
-app.exec_()
+if __name__ == "__main__":
+    # Inicia la aplicación.
+    app = MainApp()
+    app.mainloop()
